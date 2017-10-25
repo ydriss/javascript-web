e809af26aa0236e212206e4d07612ec30f3734ad
ydriss <30370338+ydriss@users.noreply.github.com>
2017-10-25 12:44:38 +0100

    Update compteurClics.js

--- chapitre_5/js/compteurClics.js
+++ chapitre_5/js/compteurClics.js
@@ -2,15 +2,19 @@
 Exercice : compter les clics
 */
 
-function clic() {
-    compteurClics++;
-    document.getElementById("compteurClics").textContent = compteurClics;
-}
+var boutonClic = document.getElementById("clic");
+
+var desactiver = document.getElementById("desactiver");
+
+function clic(){
+	compteurClics++;
+	document.getElementById("compteurClics").textContent = compteurClics;	
+	}
 
 var compteurClics = 0;
 
-document.getElementById("clic").addEventListener("click", clic);
+boutonClic.addEventListener("click", clic);
 
-document.getElementById("desactiver").addEventListener("click", function () {
-    document.getElementById("clic").removeEventListener("click", clic);
+desactiver.addEventListener("click", function () {
+	boutonClic.removeEventListener("click", clic);
 });
